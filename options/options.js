8597d9a7ff9862b8c165f377ec39ca42b0b91b5d
Crowdbotics <team@crowdbotics.com>
2024-06-04 22:49:24 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-sea-48263",
+  url: "https://weathered-sea-48263.botics.co",
+  api: "https://weathered-sea-48263.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
